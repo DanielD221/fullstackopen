67c6feb7e3e262cbe2591b8e01ddd05ec92682eb
Daniel <danielduda221@gmail.com>
2026-04-21 13:43:31 +0300

    Exercise 2.11.

--- part2/the_phonebook/src/App.jsx
+++ part2/the_phonebook/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import FilteredPersons from './components/FilteredPersons'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -37,12 +33,22 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       
-      <h2>add new note</h2>
+      <h2>add new person</h2>
       <PersonForm 
         newName={newName}
         newNumber={newNumber}
