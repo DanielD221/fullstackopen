64618e6a20f608eba6c49e706094e92e151dbd58
Daniel <danielduda221@gmail.com>
2026-06-13 22:22:17 +0300

    Exercises 3.13.-3.14.

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()// important to load dotenv before note model is imported
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -32,30 +34,41 @@ let persons = [
 
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-  const date = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+  Person.countDocuments({}).then(count => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 
@@ -74,21 +87,27 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
+  Person.findOne({ name: body.name }).then(existingPerson => {
+    if (existingPerson) {
+      response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+      return
+    }
+
+    const person = new Person({
+      name: body.name,
+      number: body.number
     })
-  }
 
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    }).catch(error => {
+      response.status(400).json({ error: error.message })
+    })
+  }).catch(error => {
+    response.status(400).json({ error: error.message })
+  })
 })
 
 
